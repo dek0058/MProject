012109bc948f80a75273af4488a9ff6cd8c0b3a7
dek0058 <editionunit@gmail.com>
2022-09-07 17:24:56 +0900

    [2022-09-07] wxWidgets 추가

---
+++ MProjectServer/TestApplication/main.cpp
@@ -0,0 +1,23 @@
+﻿#include <wx/wx.h>
+#include <wx/sizer.h>
+
+#include <wx/msw/stattext.h>
+#include <wx/msw/frame.h>
+
+class App : public wxApp {
+public:
+	bool OnInit() {
+		
+		wxFrame* window = new wxFrame(nullptr, wxID_ANY, "GUI Test");
+		//wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+		//wxStaticText* text = new wxStaticText(window, wxID_ANY, "Well Don\nEverything seems to be working", wxDefaultPosition, wxDefaultSize, wxALIGN_CENTER_HORIZONTAL);
+		//text->SetFont(wxFont(20, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
+		//sizer->Add(text);
+		
+		window->Show();
+		return true;
+	}
+
+};
+
+wxIMPLEMENT_APP(App);
